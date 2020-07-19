e52b372b57fe6b39a69236be9b42e03181a0aec3
Rajvardhan <rajthakur3619@gmail.com>
2020-07-19 20:05:48 +0530

    Database added

--- server/app.js
+++ server/app.js
@@ -1,6 +1,24 @@
 const express = require('express');
 const app = express()
+const mongoose = require('mongoose');
 const PORT = 5000
+const {MONGOURI} = require('./keys');
+
+
+//9slLiPKaWQ0GhUE5
+
+mongoose.connect(MONGOURI, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true
+})
+
+mongoose.connection.on('connected', ()=> {
+	console.log("Database connected");
+})
+
+mongoose.connection.on('error', (err)=> {
+	console.log("error on connecting to database : ", err);
+})
 
 app.get('/', (req, res)=>{
 	res.send("hello world")
