63ae525a99710e39b1248d4b67b00e3812db3136
Rajvardhan <rajthakur3619@gmail.com>
2020-07-21 00:24:45 +0530

    user added to the database with encrypted password :

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const router = express.Router()
-
+const mongoose = require('mongoose')
+const User = mongoose.model("User")
+const bcrypt = require('bcryptjs')
 
 
 router.get('/', (req, res)=> {
@@ -14,7 +16,33 @@ router.post('/signup', (req, res)=>{
 		return res.status(422).json({error:"please add all the fields"})
 	}
 
+
+	User.findOne({email:email}).then((savedUser)=>{
+		if(savedUser){
+			return res.status(422).json({err:"User already exists with this email"})
+		}
+
+		bcrypt.hash(password, 12)
+		.then(hashedPassword => {
+			const user = new User({
+				email,
+				password: hashedPassword,
+				name
+			})
+
+			user.save()
+			.then(user => {
+				res.json({message: "saved successfully"})
+			})
+			.catch(err => {
+				console.log(err)
+			})
+		})
+
+	})
+
 	res.json({message:"successfuly posted"})
 })
+
 
 module.exports = router
