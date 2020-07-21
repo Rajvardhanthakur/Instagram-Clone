95a963b083ac1667e5ae796fd3d031fcbcc39333
Rajvardhan <rajthakur3619@gmail.com>
2020-07-21 21:57:33 +0530

    JSON token updated

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -3,6 +3,8 @@ const router = express.Router()
 const mongoose = require('mongoose')
 const User = mongoose.model("User")
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const {JWT_SECRET} = require('../keys')
 
 
 router.get('/', (req, res)=> {
@@ -41,7 +43,39 @@ router.post('/signup', (req, res)=>{
 
 	})
 
+
 	res.json({message:"successfuly posted"})
+})
+
+
+
+router.post('/signin', (req, res)=>{
+	const {email, password} = req.body
+	if(!email || !password){
+		return res.status(422).json({error: "please add email or password"})
+	}
+
+	User.findOne({email:email})
+	.then(savedUser=>{
+		if(!savedUser){
+			return res.status(422).json({error: "Inavalid Email or password"})
+		}
+		bcrypt.compare(password, savedUser.password)
+		.then(doMatch => {
+			if(doMatch){
+				//res.json({message: "successfully signed in"})
+				const token = jwt.sign({_id:savedUser._id}, JWT_SECRET)
+				res.json({token})
+			}
+			else{
+				return res.status(422).json({error:"Inavalid Email or password"})
+			}
+		})
+
+	})
+	.catch(err => {
+		console.log(err)
+	})
 })
 
 
