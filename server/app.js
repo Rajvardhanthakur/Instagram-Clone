c1b312c682a7b445d4f4f1001d6fbc579711f5e7
Rajvardhan <rajthakur3619@gmail.com>
2020-07-22 13:16:02 +0530

    User's signin signup and posts related routes are added

--- server/app.js
+++ server/app.js
@@ -5,11 +5,6 @@ const PORT = 5000
 const {MONGOURI} = require('./keys');
 
 
-require('./models/user')
-
-
-app.use(express.json())
-app.use(require('./routes/auth'))
 
 mongoose.connect(MONGOURI, {
 	useNewUrlParser: true,
@@ -23,6 +18,18 @@ mongoose.connection.on('connected', ()=> {
 mongoose.connection.on('error', (err)=> {
 	console.log("error on connecting to database : ", err);
 })
+
+
+
+
+require('./models/user')
+require('./models/post')
+
+
+app.use(express.json())
+app.use(require('./routes/auth'))
+app.use(require('./routes/post'))
+
 
 app.get('/', (req, res)=>{
 	res.send("hello world")
