9b26b9b00fcea23c6b84ec55fe9891e6905880e5
Rajvardhan <rajthakur3619@gmail.com>
2020-08-02 21:01:55 +0530

    rearranging the navbarf

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,17 +1,31 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
+import React, {useContext} from 'react';
+import {Link} from 'react-router-dom';
+import {UserContext} from '../App';
 
 
 const Navbar = () => { 
+
+  const {state, dispatch} = useContext(UserContext)
+  const renderList = () => {
+    if(state){
+      return [
+          <li><Link to="/profile">profile</Link></li>,
+          <li><Link to="/create">Create Post</Link></li>
+      ]
+    }else{
+      return [
+         <li><Link to="/login">Signin</Link></li>,
+          <li><Link to="/signup">Signup</Link></li>
+      ]
+    }
+  }
+
 	return (
 	  <nav>
       <div className="nav-wrapper white">
-        <Link to="/" className="brand-logo left">Instagram</Link>
+        <Link to={state?"/":"/login"} className="brand-logo left">Instagram</Link>
         <ul id="nav-mobile" className="right">
-          <li><Link to="/login">Signin</Link></li>
-          <li><Link to="/signup">Signup</Link></li>
-          <li><Link to="/profile">profile</Link></li>
-          <li><Link to="/create">Create Post</Link></li>
+          {renderList()}
         </ul>
       </div>
     </nav>
