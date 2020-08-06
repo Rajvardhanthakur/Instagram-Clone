ec34aac8508905eb7f86dc5ce820d80729cc96bc
Rajvardhan <rajthakur3619@gmail.com>
2020-08-06 17:30:13 +0530

    deploying project

--- server/app.js
+++ server/app.js
@@ -34,7 +34,7 @@ app.use(require('./routes/user'))
 
 if(process.env.NODE_ENV=="production"){
 	app.user(express.static('client/build'))
-	const path = require("path")
+	const path = require('path')
 	app.get("*", (req,res)=>{
 		res.sendFile(path.resolve(__dirname,'client','build','index.html'))
 	})
